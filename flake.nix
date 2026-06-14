cf95798c0c77aa6e88a8bb1f648b5cbb9f0e5412
deadmade <130925709+deadmade@users.noreply.github.com>
2026-06-14 21:04:06 +0000

    chore: update boxflinger to 1.0.9

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       flake = false;
     };
     boxflinger = {
-      url = "git+https://codeberg.org/ideumi/boxflinger?ref=refs/tags/V-1.0.8";
+      url = "git+https://codeberg.org/ideumi/boxflinger?ref=refs/tags/V-1.0.9";
       flake = false;
     };
     depthfinder-src = {
@@ -35,7 +35,7 @@
 
       versions = {
         chiplang = "1.0.19";
-        boxflinger = "1.0.8";
+        boxflinger = "1.0.9";
         depthfinder = "1.0.5";
       };
 
