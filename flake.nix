6ed7d63285b0790ef0452a78474a63801a6e1726
deadmade <130925709+deadmade@users.noreply.github.com>
2026-06-21 18:22:02 +0000

    chore: update depthfinder to 1.0.7

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       flake = false;
     };
     depthfinder-src = {
-      url = "git+https://codeberg.org/ideumi/depthfinder?ref=refs/tags/V-1.0.6";
+      url = "git+https://codeberg.org/ideumi/depthfinder?ref=refs/tags/V-1.0.7";
       flake = false;
     };
   };
@@ -36,7 +36,7 @@
       versions = {
         chiplang = "1.0.21";
         boxflinger = "1.0.11";
-        depthfinder = "1.0.6";
+        depthfinder = "1.0.7";
       };
 
       makePackages =
